ce79b9e347edf33e13d29a2c386746fb6d392348
marnop <akyuziskender@hotmail.com>
2016-12-25 11:17:40 +0200

    Update a3_bottle.py

--- a3_bottle.py
+++ a3_bottle.py
@@ -1,6 +1,7 @@
-from bottle import route,run,debug,static_file,request
+from bottle import route, run, debug, static_file, request
 
 debug(True)
+
 
 def htmlify(content, style, title):
     page = """
@@ -11,8 +12,8 @@ def htmlify(content, style, title):
           """ + title + """</title>
           <meta charset="utf-8" />
           <style>
-          	.pagelocation {
-          		margin: 0px 450px;
+        	.pagelocation {
+			margin: 0px 450px;
           	}
     """ + style + """
     	  </style>
@@ -26,8 +27,9 @@ def htmlify(content, style, title):
     """
     return page
 
+
 def main_page():
-	html = """
+    html = """
 			<h1>The name of the webpage</h1>
 			<p>Please choose a type you would like to read about:</p>
 		
@@ -41,41 +43,47 @@ def main_page():
 			</div>
 			</form>
 			"""
-	style = ""
-	title = "MainPage"
-	return htmlify(html,style,title)
+    style = ""
+    title = "MainPage"
+    return htmlify(html, style, title)
+
 
 def movies():
-	html = """
+    html = """
 		<h1>The name of the webpage</h1>
+		<a href="/news_batman">
 		<div class="img1">
-		<img src="/batman.jpg" alt="Batman" width="250px">
+		    <img src="https://www.comicbookmovie.com/images/articles/147511.jpg" alt="Batman">
 		</div>
-		<div class="mar">
-		<h4><a href="/news_batman">Ben Affleck Now Indicates That He'll Be Ready To Shoot His Solo BATMAN Movie In The Spring</a></h2>
-		<h5>Our last update on the progress of The Batman didn't seem particularly positive, but it now director Ben Affleck appears 
-		to have committed to shooting the movie early next year. More past the jump...</h5>
-		</div>
+		    <h4 style="text-decoration: none">Ben Affleck Now Indicates That He'll Be Ready To Shoot His Solo BATMAN Movie In The Spring</h2>
+		    <h5 style="text-decoration: none">Our last update on the progress of The Batman didn't seem particularly positive, but it now director Ben Affleck appears
+		    to have committed to shooting the movie early next year. More past the jump...</h5>
+		    <h5>Posted by: Mark Cassidy</h5>
+		</a>
+		<hr>
 		"""
-	style = """
+    style = """
 		.img1 {
 				float: left;
+				margin-right: 20px;
 				}
-		h4 a {
+		h4 {
 				text-decoration: none;
-				margin-left: 20px;
 				color: black;
 			}
 		h5 {
 				color: gray;
-				text-indent: 20px;
 			}
+		a {
+		        text-decoration: none;
+		    }
 			"""
-	title = "Movies"
-	return htmlify(html, style, title)
+    title = "Movies"
+    return htmlify(html, style, title)
+
 
 def technology():
-	html = """
+    html = """
 		<h1>The name of the webpage</h1>
 		<div class="img1">
 		<img src="../batman.jpg" alt="Batman" width="250px">
@@ -87,7 +95,7 @@ def technology():
 		<p></p>
 		</div>
 		"""
-	style = """
+    style = """
 		.img1 {
 				float: left;
 				}
@@ -101,25 +109,27 @@ def technology():
 				text-indent: 20px;
 			}
 			"""
-	title = "Technology"
-	return htmlify(html, style, title)
+    title = "Technology"
+    return htmlify(html, style, title)
+
 
 comments = [
-	{'name' : 'Bruce Wayne',
-	 'gender' : 'Male',
-	 'age' : 35,
-	 'comment' : 'It will be an awesome movie. You can be sure guys because I am Batman.'},
-	{'name' : 'Clark Kent',
-	 'gender' : 'Male',
-	 'age' : 36,
-	 'comment' : 'I am proud of you bro. I am looking forward to see it'}
+    {'name': 'Bruce Wayne',
+     'gender': 'Male',
+     'age': 35,
+     'comment': 'It will be an awesome movie. You can be sure guys because I am Batman.'},
+    {'name': 'Clark Kent',
+     'gender': 'Male',
+     'age': 36,
+     'comment': 'I am proud of you bro. I am looking forward to see it.'}
 ]
 value = 4.30
+
+
 def news_batman():
-	rating = request.GET.get('point')
-	rating_ = str(rating)
-	html = '''
+    html = '''
 				<h2>Ben Affleck Now Indicates That He'll Be Ready To Shoot His Solo BATMAN Movie In The Spring</h2>
+				<img src="https://www.comicbookmovie.com/images/articles/banners/147511.jpg" alt="Ben Affleck">
 				<p>Our last report on the status of the standalone <strong>Batman</strong> movie made it seem like the project was still quite a while away from shooting, with director 
 				<strong>Ben Affleck</strong> appearing to suggest that the script wasn't even close to being completed.</p>
 				Well, it seems the Live by Night star has now changed his tune in that regard.</p>
@@ -152,98 +162,104 @@ def news_batman():
     						<th>Age</th>
     						<th></th>
     					</tr>'''
-	for person in comments:
-		html += '  <tr>\n'
-		html += '    <td><strong>' + str(person['name']) + '</strong></td>\n'
-		html += '    <td>' + str(person['gender']) + '</td>\n'
-		html += '    <td>' + str(person['age']) + '</td>\n'
-		html += '  </tr>\n'
-		html += '  <tr>\n'
-		html += '    <td>' + str(person['comment']) + '</td>\n'
-		html += '  </tr>\n'
-	html += '<tr>\n'
-	html += '</table><br><br>\n'
-	html +=  '<form method="POST" action="/add_submit">\n'
-	html +=  '<fieldset>\n'
-	html +=  '<legend>Add a new comment!</legend>\n'
-	html += '  Name: <input type="text" name="name" /><br />\n'
-	html += '''
+    for person in comments:
+        html += '  <tr>\n'
+        html += '    <td><strong>' + str(person['name']) + '</strong></td>\n'
+        html += '    <td>' + str(person['gender']) + '</td>\n'
+        html += '    <td>' + str(person['age']) + '</td>\n'
+        html += '  </tr>\n'
+        html += '  <tr>\n'
+        html += '    <td>' + str(person['comment']) + '</td>\n'
+        html += '  </tr>\n'
+    html += '<tr>\n'
+    html += '</table><br><br>\n'
+    html += '<form method="POST" action="/add_submit">\n'
+    html += '<fieldset>\n'
+    html += '<legend>Add a new comment!</legend>\n'
+    html += '  Name: <input type="text" name="name" /><br />\n'
+    html += '''
 		<form action="/add_submit">
   			<input type="radio" name="gender" value="Male" checked> Male<br>
   			<input type="radio" name="gender" value="Female"> Female<br>
   			<input type="radio" name="gender" value="Other"> Other<br> 
 		</form> 
 	'''
-	html += '  Age: <input type="number" name="age" /><br /><br />\n'
-	html += '  <textarea rows="5" cols="150" name="comment">'
-	html += 'Enter your comment here!</textarea>'
-	html += '  <input type="submit" value="Send the comment!"" />\n'
-	html += '  </fieldset>\n'
-	html += '</form>\n'
-	html += '</fieldset>'
-	style = """
+    html += '  Age: <input type="number" name="age" /><br /><br />\n'
+    html += '  <textarea rows="5" cols="127" name="comment">'
+    html += 'Enter your comment here!</textarea>'
+    html += '  <input type="submit" value="Send the comment!"" />\n'
+    html += '  </fieldset>\n'
+    html += '</form>\n'
+    html += '</fieldset>'
+    style = """
 			"""
-	title = "Ben Affleck Now Indicates That He'll Be Raedy To Shoot His Solo BATMAN Movie In The Spring"
-	return htmlify(html, style, title)
+    title = "Ben Affleck Now Indicates That He'll Be Raedy To Shoot His Solo BATMAN Movie In The Spring"
+    return htmlify(html, style, title)
+
+
 ini = 7
 tot = 8
+
+
 def rate_submit():
-	title ="Your Rating"
+    title = "Your Rating"
 
-	post_request = request.POST
-	rate = int(post_request['quantity'])
-	global value, ini, tot
-	value = float(((ini*float(value))+int(rate)) / tot)
-	value = format(value, '.2f')
-	ini += 1
-	tot += 1
+    post_request = request.POST
+    rate = int(post_request['quantity'])
+    global value, ini, tot
+    value = float(((ini * float(value)) + int(rate)) / tot)
+    value = format(value, '.2f')
+    ini += 1
+    tot += 1
 
-	html = "<p>Your rating for the post is " + str(rate) + """.</p>
+    html = "<p>Your rating for the post is " + str(rate) + """.</p>
 			<form action="/news_batman">
    				<input type="submit" value="Turn back to the post!">
     		</form>
     		"""
-	style = ""
-	return htmlify(html, style, title)
+    style = ""
+    return htmlify(html, style, title)
+
 
 def add_submit():
-	post_request = request.POST
+    post_request = request.POST
 
-	name = str(post_request['name'])
-	gender = str(post_request['gender'])
-	age = int(post_request['age'])
-	comment = str(request.forms.get('comment'))
+    name = str(post_request['name'])
+    gender = str(post_request['gender'])
+    age = int(post_request['age'])
+    comment = str(request.forms.get('comment'))
 
-	global comments
-	comments += [ {'name' : name, 'gender' : gender, 'age' : age, 'comment' : comment} ]
-	
-	html = '''<p>You can see your comment below</p>
+    global comments
+    comments += [{'name': name, 'gender': gender, 'age': age, 'comment': comment}]
+
+    html = '''<p>You can see your comment below</p>
 			<table>\n
 			<tr>\n
- 				<th>Name</th>\n
-				<th>Gender</th>\n
+                <th>Name</th>\n
+                <th>Gender</th>\n
 				<th>Age</th>\n
-  				<th></th>\n
-  			</tr>\n
-  			<tr>\n
+                <th></th>\n
+            </tr>\n
+            <tr>\n
  			<td><strong>''' + str(name) + '''</strong></td>\n    
  			<td>''' + str(gender) + '</td>\n' + '<td>' + str(age) + '''</td>\n
-    		</tr>\n
-   			<tr>\n
+            </tr>\n
+            <tr>\n
    			<td>''' + str(comment) + '</td>\n  </tr>\n </table>' + """
-			<form action="/news_batman">
-   				<input type="submit" value="Turn back to the comments!">
-    		</form>
-    		"""
+            <form action="/news_batman">
+                <input type="submit" value="Turn back to the comments!">
+            </form>
+            """
 
-	style = ""
-	title = "Comments - Ben Affleck Now Indicates That He'll Be Raedy To Shoot His Solo BATMAN Movie In The Spring"
-	return htmlify(html, style, title)
+    style = ""
+    title = "Comments - Ben Affleck Now Indicates That He'll Be Raedy To Shoot His Solo BATMAN Movie In The Spring"
+    return htmlify(html, style, title)
 
-route('/mainpage','GET',main_page)
-route('/movies','GET',movies)
-route('/technology','GET',technology)
-route('/news_batman','GET',news_batman)
-route('/add_submit','POST',add_submit)
-route('/rate_submit','POST',rate_submit)
+
+route('/mainpage', 'GET', main_page)
+route('/movies', 'GET', movies)
+route('/technology', 'GET', technology)
+route('/news_batman', 'GET', news_batman)
+route('/add_submit', 'POST', add_submit)
+route('/rate_submit', 'POST', rate_submit)
 run()
